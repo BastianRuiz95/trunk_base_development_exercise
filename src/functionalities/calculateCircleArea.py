8a0750a0c361f91cb378a012a42b9bf4d5290035
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2024-05-10 12:53:54 -0400

    Feat: calculate circle area function

--- src/functionalities/calculateCircleArea.py
+++ src/functionalities/calculateCircleArea.py
@@ -1,14 +1,29 @@
 import math
 
-def calcular_area_circulo(radio):
-    area = math.pi * radio ** 2
-    return area
+def calculateArea(radio):
+    if radio == "":
+        raise Exception("The input must not be empty")
+    elif radio.isalpha():
+        raise Exception("The input is invalid, please enter a number")
+    elif float(radio) <= 0:
+        raise Exception("The input must be greater than 0")
+    return round(math.pi * pow(float(radio),2),2)
 
-# Solicitar al usuario que ingrese el radio
-radio = float(input("Por favor, ingresa el radio del círculo: "))
 
-# Calcular el área del círculo
-area_del_circulo = calcular_area_circulo(radio)
+def printResult(result: float):
+    print("\nCircle area:")
+    print(result)
+    print()
 
-# Imprimir el resultado
-print("El área del círculo con radio", radio, "es:", area_del_circulo)
+
+def execute():
+    circleRadius = input("\nDefine the radius of the circle: ")
+
+    try:
+        area = calculateArea(circleRadius)
+    except Exception as e:
+        print("\n" + e.__str__())
+        print()
+        return
+    
+    printResult(area)
