d4c117c0ef8dbfba7c55bc224e70f1878a095be3
Maria Luisa <maria.luisarivera22@gmail.com>
2024-05-09 20:43:34 -0400

    fix: Added empty list validation on function

--- src/functionalities/calculateAverage.py
+++ src/functionalities/calculateAverage.py
@@ -1,3 +1,9 @@
 def calcular_promedio(valores):
+    try:
+        if len(valores) == 0:
+            return 0
+    except TypeError:
+        print("Error: Lista vacía")
+        return 0
     total = sum(valores)
     return total / len(valores)
